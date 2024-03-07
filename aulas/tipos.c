cc084fadb60c77017ed370ddb91b7d9782fec29f
ISAACAGUIAR777 <160795587+ISAACAGUIAR777@users.noreply.github.com>
2024-03-07 00:50:08 +0000

    codigos da aula02 pratica01

---
+++ aulas/tipos.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+int main() {
+  char caracter; // 'A' '1' '\n' '\\' 1byte -128 a 127
+  int inteiro; // 123456              4bytes -bi a 2bi
+  float decimal_com_6casas; // 1.23456 4bytes -3.4a38 a 3.4a38
+  double decimal_com_15casas; // 0.000000000000 8bytes -1.8a308
+    a 1.8a308
+  void sem_valor;
+  // nao existe string, mas C aceita "uma frase qualquer"
+  return 0;
+}
